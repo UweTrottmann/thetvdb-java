93bb276a1990f80fa88c6c72bb7d1e1cf7e62bf9
Uwe Trottmann <uwe.trottmann@gmail.com>
2015-05-06 10:54:50 +0200

    Add API key error when testing login.

--- src/test/java/com/uwetrottmann/thetvdb/services/AuthenticationTest.java
+++ src/test/java/com/uwetrottmann/thetvdb/services/AuthenticationTest.java
@@ -12,6 +12,10 @@ public class AuthenticationTest extends BaseTestCase {
 
     @Test
     public void test_login() {
+        if (API_KEY.length() == 0) {
+            throw new IllegalArgumentException("Set your TheTVDB API key to test login.");
+        }
+
         Token token = getTheTvdb().authentication().login(new LoginData(API_KEY));
         System.out.println("Retrieved token: " + token.token + " (valid for 1 hour)");
     }
