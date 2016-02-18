f53e0b6ef0f6ad02a745e94733b4fb8cae88723f
Uwe Trottmann <uwe.trottmann@gmail.com>
2016-02-18 10:56:43 +0100

    Update Authentication service and test to retrofit2.

--- src/main/java/com/uwetrottmann/thetvdb/services/Authentication.java
+++ src/main/java/com/uwetrottmann/thetvdb/services/Authentication.java
@@ -2,9 +2,10 @@ package com.uwetrottmann.thetvdb.services;
 
 import com.uwetrottmann.thetvdb.entities.LoginData;
 import com.uwetrottmann.thetvdb.entities.Token;
-import retrofit.http.Body;
-import retrofit.http.GET;
-import retrofit.http.POST;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
 
 public interface Authentication {
 
@@ -13,13 +14,13 @@ public interface Authentication {
      * for all subsequent requests and user auth is required for routes in the User section.
      */
     @POST("/login")
-    Token login(@Body LoginData loginData);
+    Call<Token> login(@Body LoginData loginData);
 
     /**
      * Refreshes your current, valid JWT token and returns a new token. Hit this route so that you do not have to post
      * to /login with your API key and credentials once you have already been authenticated.
      */
     @GET("/refresh_token")
-    Token refreshToken();
+    Call<Token> refreshToken();
 
 }
--- src/test/java/com/uwetrottmann/thetvdb/services/AuthenticationTest.java
+++ src/test/java/com/uwetrottmann/thetvdb/services/AuthenticationTest.java
@@ -4,6 +4,9 @@ import com.uwetrottmann.thetvdb.BaseTestCase;
 import com.uwetrottmann.thetvdb.entities.LoginData;
 import com.uwetrottmann.thetvdb.entities.Token;
 import org.junit.Test;
+import retrofit2.Call;
+
+import java.io.IOException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -12,7 +15,7 @@ public class AuthenticationTest extends BaseTestCase {
     private static final String API_KEY = "";
 
     @Test
-    public void test_login() {
+    public void test_login() throws IOException {
         if (API_KEY.length() == 0) {
             throw new IllegalArgumentException("Set your TheTVDB API key to test /login.");
         }
@@ -20,12 +23,14 @@ public class AuthenticationTest extends BaseTestCase {
         // remove existing auth
         getTheTvdb().setJsonWebToken(null);
 
-        Token token = getTheTvdb().authentication().login(new LoginData(API_KEY));
+        Call<Token> call = getTheTvdb().authentication().login(new LoginData(API_KEY));
+
+        Token token = call.execute().body();
         System.out.println("Retrieved token: " + token.token + " (valid for 1 hour)");
     }
 
     @Test
-    public void test_refreshToken() {
+    public void test_refreshToken() throws IOException {
         if (API_KEY.length() == 0) {
             throw new IllegalArgumentException("Set your TheTVDB API key to test /refresh_token.");
         }
@@ -34,10 +39,12 @@ public class AuthenticationTest extends BaseTestCase {
         getTheTvdb().setJsonWebToken(null);
 
         // authenticate with API key
-        Token token = getTheTvdb().authentication().login(new LoginData(API_KEY));
+        Call<Token> call = getTheTvdb().authentication().login(new LoginData(API_KEY));
+        Token token = call.execute().body();
         getTheTvdb().setJsonWebToken(token.token);
 
-        Token refreshedToken = getTheTvdb().authentication().refreshToken();
+        call = getTheTvdb().authentication().refreshToken();
+        Token refreshedToken = call.execute().body();
         assertThat(refreshedToken.token).isNotEqualTo(token.token);
         System.out.println("Refreshed token: " + refreshedToken.token + " (valid for 1 hour)");
     }
