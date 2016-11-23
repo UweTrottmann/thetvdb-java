af3986622f7cf764c4c0b06a76657b7a13f5b581
Uwe Trottmann <uwe+git@uwetrottmann.com>
2016-11-23 09:23:17 +0100

    Add `siteRatingCount` to Series. #9
    - Add docs.
    - Change `lastUpdated` to `Integer`, was `Long`.

--- src/main/java/com/uwetrottmann/thetvdb/entities/Series.java
+++ src/main/java/com/uwetrottmann/thetvdb/entities/Series.java
@@ -17,25 +17,36 @@ public class Series {
     public Integer id;
     public String seriesName;
     public List<String> aliases = new ArrayList<>();
+    /** Image path suffix, like "graphical/83462-g20.jpg". */
     public String banner;
     /**
      * A string containing either "Ended" or "Continuing". Can be null.
      */
     public String status;
+    /** ISO 8601 date string, like "2010-09-20". */
     public String firstAired;
     public String network;
     public String networkId;
+    /** In minutes. */
     public String runtime;
     public List<String> genre = new ArrayList<>();
     public String overview;
-    public Long lastUpdated;
+    /** Time in seconds, like 1430845514. */
+    public Integer lastUpdated;
+    /** An English day string, like "Monday". */
     public String airsDayOfWeek;
+    /** In most cases a AM/PM time string, like "9:00 PM". Good luck with this. */
     public String airsTime;
+    /** US rating, like "TV-MA". */
     public String rating;
     public String imdbId;
     public String zap2itId;
+    /** ISO 8601 date-time string, like "2010-09-20 15:05:50". */
     public String added;
+    /** TheTVDB user id. */
     public Integer addedBy;
+    /** Value from 0.0 to 10.0. */
     public Double siteRating;
+    public Integer siteRatingCount;
 
 }
