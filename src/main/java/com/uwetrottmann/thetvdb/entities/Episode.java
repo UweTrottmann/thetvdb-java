39062cf9f4fae2020e68cebd98ff655b90e4bdf2
Manuel Laggner <manuel.laggner@gmail.com>
2018-07-22 10:25:56 +0200

    removed FullEpisode usage
    because the service /series/{id}/episodes now respond with full episode data
    https://gitlab.thetvdb.com/site/thetvdb_api/issues/28
    
    Signed-off-by: Manuel Laggner <manuel.laggner@gmail.com>

--- src/main/java/com/uwetrottmann/thetvdb/entities/Episode.java
+++ src/main/java/com/uwetrottmann/thetvdb/entities/Episode.java
@@ -20,36 +20,34 @@ public class Episode {
     public Long lastUpdated;
     public String overview;
 
+    public Integer airsAfterSeason;
+    public Integer airsBeforeEpisode;
+    public Integer airsBeforeSeason;
+    public List<String> directors;
+    public String dvdChapter;
+    public String dvdDiscid;
+    /** Episode image path suffix, like "episodes/83462/398671.jpg". */
+    public String filename;
+    public List<String> guestStars;
+    public String imdbId;
+    /** TheTVDB user id. */
+    public Integer lastUpdatedBy;
+    public String productionCode;
+    /** TheTVDB series id. */
+    public Integer seriesId;
+    public String showUrl;
+    /** Value from 0.0 to 10.0. */
+    public Double siteRating;
+    public Integer siteRatingCount;
+    public String thumbAdded;
+    /** TheTVDB user id. */
+    public Integer thumbAuthor;
+    public String thumbWidth;
+    public String thumbHeight;
+    public List<String> writers;
+
     public static class Translations {
         public String episodeName;
         public String overview;
-    }
-
-    public static class FullEpisode extends Episode {
-        public Integer airsAfterSeason;
-        public Integer airsBeforeEpisode;
-        public Integer airsBeforeSeason;
-        public List<String> directors;
-        public String dvdChapter;
-        public String dvdDiscid;
-        /** Episode image path suffix, like "episodes/83462/398671.jpg". */
-        public String filename;
-        public List<String> guestStars;
-        public String imdbId;
-        /** TheTVDB user id. */
-        public Integer lastUpdatedBy;
-        public String productionCode;
-        /** TheTVDB series id. */
-        public Integer seriesId;
-        public String showUrl;
-        /** Value from 0.0 to 10.0. */
-        public Double siteRating;
-        public Integer siteRatingCount;
-        public String thumbAdded;
-        /** TheTVDB user id. */
-        public Integer thumbAuthor;
-        public String thumbWidth;
-        public String thumbHeight;
-        public List<String> writers;
     }
 }
